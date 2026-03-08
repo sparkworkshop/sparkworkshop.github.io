3e93dd386ee12b7254fb9d86aa930ac68aba91ca
anjomshoaa <amin.anjomshoaa@wu.ac.at>
2026-03-08 23:01:43 +0100

    Title updated.

--- src/navigation.ts
+++ src/navigation.ts
@@ -55,6 +55,6 @@ export const footerData = {
 		{ ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
 	],
 	footNote: `
-		Made by <a class="text-blue-600 underline dark:text-muted" href="https://github.com/arthelokyo"> Arthelokyo</a> · All rights reserved.
+	Copyright © SPARK 2026
 	`,
 };
